48e9bc8f22b91275ada1f9a4de3fa6331ae87d2c
Florian Reintgen <85225228+TheReal-Flo@users.noreply.github.com>
2026-06-05 22:00:10 +0200

    Initial commit

---
+++ src/client/java/dev/justfeli/vtm/client/MainModClient.java
@@ -0,0 +1,10 @@
+package dev.justfeli.vtm.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/justfeli/vtm/MainMod.java
@@ -0,0 +1,10 @@
+package dev.justfeli.vtm;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MainMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
